2c5da59a74a30c9558c457f021d290b93704c9ce
sagun <sagun_90@hotmail.com>
2017-12-20 16:18:49 -0600

    Added packages and car class

---
+++ src/com/springweb/model/Car.java
@@ -0,0 +1,32 @@
+package com.springweb.model;
+
+public class Car {
+	private int carId;
+	private String manufacturer;
+	private int model;
+	
+	public int getCarId() {
+		return carId;
+	}
+	
+	public void setCarId(int carId) {
+		this.carId = carId; 
+	}
+	
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+	public int getModel() {
+		return model;
+	}
+	
+	public void setModel(int model) {
+		this.model = model; 
+	}
+	
+}
